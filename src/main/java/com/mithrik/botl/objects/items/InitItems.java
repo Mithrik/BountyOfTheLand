116b4a507ec5d7afae2b4be5d7ffe9040a87b5b4
Daniel Soto <dsd19951208@hotmail.com>
2020-04-01 21:19:03 -0600

    Update 0.1.0
    Added three food items: cooked rice, phulka and rye bread.
    
    Added recipe unlocks for all existing recipes.
    
    Updated textures for the mango, peach and yam.

--- src/main/java/com/mithrik/botl/objects/items/InitItems.java
+++ src/main/java/com/mithrik/botl/objects/items/InitItems.java
@@ -30,8 +30,11 @@ public class InitItems {
 	public static final Item MILLET = null;
 	public static final Item YAM = null;
 	
-	public static final Item PEACH = null;
+	public static final Item COOKED_RICE = null;
 	public static final Item MANGO = null;
+	public static final Item PEACH = null;
+	public static final Item PHULKA = null;
+	public static final Item RYE_BREAD = null;
 	
 	public static final Item CARROT_SEEDS = null;
 	public static final Item CORN_SEEDS = null;
@@ -48,24 +51,26 @@ public class InitItems {
 		IForgeRegistry<Item> r = event.getRegistry();
 		
 		// SIMPLE ITEMS
-		register(r,"millet",new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"rice",new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"rye",new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"millet", new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"rice", new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"rye", new CropItem(new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
 		
 		// FOOD ITEMS
-		register(r,"corn",new CropItem(new Item.Properties().food(makeFood(2, 0.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"yam",new CropItem(new Item.Properties().food(makeFood(4, 1.2f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"corn", new CropItem(new Item.Properties().food(makeFood(2, 0.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"yam", new CropItem(new Item.Properties().food(makeFood(4, 1.2f)).group(ItemGroupBotL.BOTL_ITEMS)));
 		
-		register(r,"peach",new Item(new Item.Properties().food(makeFood(4, 2.4f)).group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"mango",new Item(new Item.Properties().food(makeFood(4, 3.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"cooked_rice", new Item(new Item.Properties().food(makeFood(5, 3.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"mango", new Item(new Item.Properties().food(makeFood(4, 3.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"peach", new Item(new Item.Properties().food(makeFood(4, 2.4f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"phulka", new Item(new Item.Properties().food(makeFood(3, 3.6f)).group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"rye_bread", new Item(new Item.Properties().food(makeFood(5, 2.4f)).group(ItemGroupBotL.BOTL_ITEMS)));
 		
 		// BLOCK-NAMED ITEMS
-
-		register(r,"corn_seeds",new SeedItem(InitBlocks.CROP_CORN, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"millet_seeds",new SeedItem(InitBlocks.CROP_MILLET, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"rice_seeds",new SeedItem(InitBlocks.CROP_RICE, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"rye_seeds",new SeedItem(InitBlocks.CROP_RYE, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
-		register(r,"yam_seeds",new SeedItem(InitBlocks.CROP_YAMS, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"corn_seeds", new SeedItem(InitBlocks.CROP_CORN, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"millet_seeds", new SeedItem(InitBlocks.CROP_MILLET, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"rice_seeds", new SeedItem(InitBlocks.CROP_RICE, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"rye_seeds", new SeedItem(InitBlocks.CROP_RYE, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
+		register(r,"yam_seeds", new SeedItem(InitBlocks.CROP_YAMS, new Item.Properties().group(ItemGroupBotL.BOTL_ITEMS)));
 	}
 	
 	// === OBJECT TEMPLATES ===
